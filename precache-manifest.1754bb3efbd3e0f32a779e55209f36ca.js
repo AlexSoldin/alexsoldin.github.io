c661b7bdae088a3974eeb91ac327eea3f17a7b4a
Alex Soldin <alex.soldin@gmail.com>
2020-08-15 19:59:30 +0200

    Updated structure

---
+++ precache-manifest.1754bb3efbd3e0f32a779e55209f36ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17531ef37be14e9d7797186579b49138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "148bb22699d48fb696f4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "03d65cfe2ff6a7f131a0",
+    "url": "/static/js/2.4e63a2bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e63a2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148bb22699d48fb696f4",
+    "url": "/static/js/main.fbbd1366.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "12ed0b26b38614fec59b3ae8ca87f28d",
+    "url": "/static/media/CV.12ed0b26.pdf"
+  },
+  {
+    "revision": "1167a972f7f131b5643a513bfdbddb79",
+    "url": "/static/media/Transcript.1167a972.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.f7f18fb2e7e842071a193e5ef713fd77.js"
+  "/precache-manifest.1754bb3efbd3e0f32a779e55209f36ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
